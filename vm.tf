d4a2a4d90f9b85610c2c54f6e696883b92dcdff5
n4devops <114721662+n4devops@users.noreply.github.com>
2022-11-08 18:40:09 +0530

    Create vm.tf

---
+++ vm.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "web" {
+    ami           = "ami-0e6329e222e662a52"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.jenk-sg.id]
+ 
+  tags = {
+    Name = "pyvm1"
+  }
+}
+resource "aws_security_group" "jenk-sg" {
+  name = "j-grp"
+  description = "Allow HTTP  traffic via Terraform"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+ output "ec2_instance_web" {
+  description = "EC2 Instance Public IP"
+  value = aws_instance.web.public_ip
+}
